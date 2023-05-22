5f2b40afad864f4b95f5c50b549db7e5380a6479
skyzavr <dankozero111@gmail.com>
2023-05-22 23:34:49 +0300

    Add: switching between color types with memorizing the number of cards shown

--- src/components/pages/ImageColour/imagePalette/ColourSwitching/ColourSwitching.js
+++ src/components/pages/ImageColour/imagePalette/ColourSwitching/ColourSwitching.js
@@ -2,12 +2,19 @@ import { useState, useContext, useEffect } from 'react';
 import classes from './ColourSwitching.module.css';
 import ColourCardList from '../list/ColourCardList';
 import { ThemeContext } from '../../../../../App';
+
 const ColourSwitching = ({ light, dark, coloured }) => {
   const { theme } = useContext(ThemeContext);
+  const defaultAmount = 20;
   const [colourBtn, setCOlourBtn] = useState({
     light: true,
     dark: false,
     coloured: false,
+  });
+  const [cardAmount, setCardAmount] = useState({
+    light: defaultAmount,
+    dark: defaultAmount,
+    coloured: defaultAmount,
   });
   const [currentType, setCurrentType] = useState('light');
   const [defaulColour, setDefaultCOlour] = useState(
@@ -29,31 +36,78 @@ const ColourSwitching = ({ light, dark, coloured }) => {
   const setColour = (param) => {
     return currentType === param ? '#3787ff' : defaulColour;
   };
+  const updateCardAmountHandler = (data) => {
+    const { value, type } = data;
+    if (type === 'light')
+      setCardAmount({
+        light: value,
+        dark: cardAmount.dark,
+        coloured: cardAmount.coloured,
+      });
+    else if (type === 'dark')
+      setCardAmount({
+        light: cardAmount.light,
+        dark: value,
+        coloured: cardAmount.coloured,
+      });
+    else
+      setCardAmount({
+        light: cardAmount.light,
+        dark: cardAmount.dark,
+        coloured: value,
+      });
+  };
   return (
     <div className={classes.wrapper}>
-      <div className={classes.btns}>
-        <div
-          onClick={() => colourSwitchHandler('light')}
-          style={{ color: `${setColour('light')}` }}
-        >
-          Light
+      {[light, dark, coloured].some((el) => el.length !== 0) && (
+        <div className={classes.btns}>
+          <div
+            onClick={() => colourSwitchHandler('light')}
+            style={{ color: `${setColour('light')}` }}
+          >
+            Light
+          </div>
+          <div
+            onClick={() => colourSwitchHandler('dark')}
+            style={{ color: `${setColour('dark')}` }}
+          >
+            Dark
+          </div>
+          <div
+            onClick={() => colourSwitchHandler('coloured')}
+            style={{ color: `${setColour('coloured')}` }}
+          >
+            Coloured
+          </div>
         </div>
-        <div
-          onClick={() => colourSwitchHandler('dark')}
-          style={{ color: `${setColour('dark')}` }}
-        >
-          Dark
-        </div>
-        <div
-          onClick={() => colourSwitchHandler('coloured')}
-          style={{ color: `${setColour('coloured')}` }}
-        >
-          Coloured
-        </div>
-      </div>
-      {colourBtn.light && <ColourCardList list={light} />}
-      {colourBtn.dark && <ColourCardList list={dark} />}
-      {colourBtn.coloured && <ColourCardList list={coloured} />}
+      )}
+      {colourBtn.light && (
+        <ColourCardList
+          list={light}
+          length={cardAmount.light}
+          param={'light'}
+          defaultAmount={defaultAmount}
+          onShowHandler={updateCardAmountHandler}
+        />
+      )}
+      {colourBtn.dark && (
+        <ColourCardList
+          list={dark}
+          length={cardAmount.dark}
+          param={'dark'}
+          defaultAmount={defaultAmount}
+          onShowHandler={updateCardAmountHandler}
+        />
+      )}
+      {colourBtn.coloured && (
+        <ColourCardList
+          list={coloured}
+          length={cardAmount.coloured}
+          param={'coloured'}
+          defaultAmount={defaultAmount}
+          onShowHandler={updateCardAmountHandler}
+        />
+      )}
     </div>
   );
 };
--- src/components/pages/ImageColour/imagePalette/list/ColourCardList.js
+++ src/components/pages/ImageColour/imagePalette/list/ColourCardList.js
@@ -1,21 +1,71 @@
+import { useEffect, useState } from 'react';
 import classes from './ColourCardList.module.css';
 import MoreInfoIcon from './MoreInfoIcon';
 import Card from './card/Card';
+import Button from '../../../../ui/btn/button/Button';
 
-const ColourCardList = ({ list }) => {
+const ColourCardList = ({
+  list,
+  length,
+  onShowHandler,
+  param,
+  defaultAmount,
+}) => {
+  const [listLen, setListLen] = useState(
+    list.length < length ? list.length : length
+  );
   const moreInfoHadnler = () => {};
+  const ShowMore = () => {
+    const value = listLen + defaultAmount;
+    const newValue = value >= list.length ? list.length : value;
+    setListLen(newValue);
+    onShowHandler({ value: newValue, type: param });
+  };
+  const showMoreInfo = () => {
+    return listLen + defaultAmount >= list.length
+      ? list.length - listLen
+      : defaultAmount;
+  };
+  const updateListHadler = (value) => {
+    setListLen(value);
+    onShowHandler({ value: defaultAmount, type: param });
+  };
   return (
-    <ul className={classes.cardList}>
-      {list.map((el) => (
-        <li key={el}>
-          <Card bgrColour={el} />
-          <div className={classes.param}>
-            <p>{el}</p>
-            <MoreInfoIcon clickHandler={moreInfoHadnler} />
-          </div>
-        </li>
-      ))}
-    </ul>
+    <div className={classes.wrapper}>
+      <ul className={classes.cardList}>
+        {list.slice(0, listLen).map((el) => (
+          <li key={el}>
+            <Card bgrColour={el} />
+            <div className={classes.param}>
+              <p>{el}</p>
+              <MoreInfoIcon clickHandler={moreInfoHadnler} />
+            </div>
+          </li>
+        ))}
+      </ul>
+      {list.length > defaultAmount && (
+        <div className={classes.btns}>
+          {listLen !== list.length && (
+            <Button
+              text={`Show more ${showMoreInfo()} cards`}
+              onClickFunc={ShowMore}
+            />
+          )}
+          {listLen !== list.length && (
+            <Button
+              text={`Show all (${list.length - listLen})`}
+              onClickFunc={() => updateListHadler(list.length)}
+            />
+          )}
+          {listLen > defaultAmount && (
+            <Button
+              text="Collapse"
+              onClickFunc={() => updateListHadler(defaultAmount)}
+            />
+          )}
+        </div>
+      )}
+    </div>
   );
 };
 export default ColourCardList;
