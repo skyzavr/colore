ed9f07fe536819c8679986a8cf4e0d9a3619931b
skyzavr <dankozero111@gmail.com>
2023-05-26 15:18:17 +0300

    update:use hook to get the current colour to clipboard

--- src/components/pages/ImageColour/imagePalette/list/card/Card.js
+++ src/components/pages/ImageColour/imagePalette/list/card/Card.js
@@ -1,35 +1,11 @@
-import { useContext, useRef } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import classes from './Card.module.css';
 import { ThemeContext } from '../../../../../../App';
 import { contrastColour } from '../../../../../../convertFunctions';
-import { ColourContext } from '../../../../../../App';
+import { useCopyClipboard } from '../../../../../../hooks/useCopyClipboard';
 const Card = ({ bgrColour }) => {
   const { theme } = useContext(ThemeContext);
-  const { setColour } = useContext(ColourContext);
-  const CardRef = useRef();
-  const copyColorHandler = () => {
-    const cardTop = CardRef.current.offsetTop;
-    const text = bgrColour;
-    let type, message;
-    async function copyToClipboard(msg) {
-      try {
-        await navigator.clipboard.writeText(msg);
-        type = 'success';
-        message = `The colour ${msg} has been copied!`;
-      } catch {
-        type = 'error';
-        message = `There is some problem...`;
-      }
-      return { type, message };
-    }
-    copyToClipboard(text).then((el) => {
-      setColour({
-        ...el,
-        id: Math.floor(Math.random() * 10000),
-        cardTop,
-      });
-    });
-  };
+  const [copy] = useCopyClipboard();
   const setBorder = () => {
     const currentTheme = theme === 'light' ? '#ffffff' : '#131313';
     const color = contrastColour(currentTheme, false);
@@ -37,7 +13,7 @@ const Card = ({ bgrColour }) => {
     return rgba;
   };
   return (
-    <div className={classes.wrapper} onClick={copyColorHandler} ref={CardRef}>
+    <div className={classes.wrapper} onClick={() => copy(bgrColour)}>
       <div
         className={classes.card}
         style={{
