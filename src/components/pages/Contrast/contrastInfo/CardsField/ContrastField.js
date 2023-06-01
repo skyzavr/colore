387fbc9a70377cfc7bb3c5d983283c5b6d01c125
skyzavr <dankozero111@gmail.com>
2023-06-01 14:46:07 +0300

    update contrast ratio with sepparate hook

--- src/components/pages/Contrast/contrastInfo/CardsField/ContrastField.js
+++ src/components/pages/Contrast/contrastInfo/CardsField/ContrastField.js
@@ -1,58 +1,33 @@
 import { useState, useEffect } from 'react';
 import Card from '../contrastCard/Card';
 import classes from './ContrastField.module.css';
-import { hexToDec } from '../../../../../convertFunctions';
-
+import { useCalculateContrastRatio } from '../../../../../hooks/useCalculateContrastRatio';
 const ContrastField = ({ colour, bgrColour }) => {
-  const contrastMark = () => {
-    if (conrastRatio >= 12) return 'Great';
-    if (conrastRatio >= 7) return 'Good';
+  const [getRatio] = useCalculateContrastRatio();
+  const contrastMarkHandler = () => {
+    if (contrastRatio >= 12) return 'Great';
+    if (contrastRatio >= 7) return 'Good';
     return 'Poor';
   };
-  const additionalParam = (value) => {
-    return (value + 0.055) / 1.055;
-  };
-  const getParam = (value) => {
-    return value <= 0.03928
-      ? value / 12.92
-      : Math.pow(additionalParam(value), 2.4);
-  };
-  const getRGB = (hex) => {
-    const arr = [hex.slice(1, 3), hex.slice(3, 5), hex.slice(5)].map(
-      (el) => hexToDec(el) / 255
-    );
-    return arr.map((el) => getParam(el));
-  };
-  const getLuminance = (color) => {
-    const rgbArr = getRGB(color);
-    const Luminance =
-      rgbArr[0] * 0.2126 + rgbArr[1] * 0.7152 + rgbArr[2] * 0.0722;
-    return Luminance;
-  };
-  const getRatio = () => {
-    const Lum1 = getLuminance(colour),
-      Lum2 = getLuminance(bgrColour);
-    const L1 = Math.max(Lum1, Lum2);
-    const L2 = Math.min(Lum1, Lum2);
-    return (L1 + 0.05) / (L2 + 0.05);
-  };
-  const [conrastRatio, setContrastRatio] = useState(getRatio());
-  const [conrastMark, setContrastMark] = useState(getRatio());
+  const [contrastRatio, setContrastRatio] = useState(
+    getRatio(colour, bgrColour)
+  );
+  const [contrastMark, setContrastMark] = useState(getRatio(colour, bgrColour));
   useEffect(() => {
-    setContrastRatio(getRatio());
-    setContrastMark(contrastMark());
+    setContrastRatio(getRatio(colour, bgrColour));
+    setContrastMark(contrastMarkHandler());
   }, [colour, bgrColour]);
   return (
     <div className={classes.wrapper}>
       <div
-        className={classes[`ContrastCard`] + ' ' + classes[`${conrastMark}`]}
+        className={classes[`ContrastCard`] + ' ' + classes[`${contrastMark}`]}
       >
         <div className={classes.info}>Contrast</div>
-        <div className={classes.value}>{conrastRatio.toFixed(1)}</div>
-        <div className={classes.desc}>{conrastMark}</div>
+        <div className={classes.value}>{contrastRatio.toFixed(1)}</div>
+        <div className={classes.desc}>{contrastMark}</div>
       </div>
-      <Card title="Large text" ratio={conrastRatio} AA="3" AAA="4.5" />
-      <Card title="Standard text" ratio={conrastRatio} AA="4.5" AAA="7" />
+      <Card title="Large text" ratio={contrastRatio} AA="3" AAA="4.5" />
+      <Card title="Standard text" ratio={contrastRatio} AA="4.5" AAA="7" />
     </div>
   );
 };
