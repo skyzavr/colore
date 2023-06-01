c4c6b28cbeebb094a24913d3942fd949fcc23230
skyzavr <dankozero111@gmail.com>
2023-06-01 14:49:25 +0300

    add some more funcs, that convert colour from one system to another

--- src/convertFunctions.js
+++ src/convertFunctions.js
@@ -69,6 +69,52 @@ export const rgbToHex = (huePos, satPos, lightPos) => {
   }
   return result;
 };
+export const rgbToHSL = (rgb) => {
+  let R = rgb[0] / 255;
+  let G = rgb[1] / 255;
+  let B = rgb[2] / 255;
+  const Cmax = Math.max(R, G, B);
+  const Cmin = Math.min(R, G, B);
+  const delta = Cmax - Cmin;
+  let s, l, h;
+  l = (Cmax + Cmin) / 2;
+  delta === 0 ? (s = 0) : (s = delta / (1 - Math.abs(2 * l - 1)));
+  if (delta === 0) h = 0;
+  else if (Cmax === R) h = 60 * (((G - B) / delta) % 6);
+  else if (Cmax === G) h = 60 * ((B - R) / delta + 2);
+  else h = 60 * ((R - G) / delta + 4);
+  h = Math.floor(h);
+  s = Number((s * 100).toFixed(1));
+  l = Number((l * 100).toFixed(1));
+  return [h, s, l];
+};
+export const rgbToSl = (red, green, blue) => {
+  let R = red / 255;
+  let G = green / 255;
+  let B = blue / 255;
+  const Cmax = Math.max(R, G, B);
+  const Cmin = Math.min(R, G, B);
+  const delta = Cmax - Cmin;
+  let s, l;
+  l = (Cmax + Cmin) / 2;
+  delta === 0 ? (s = 0) : (s = delta / (1 - Math.abs(2 * l - 1)));
+  return [s, l];
+};
+export const rgbToCMyk = (rgb) => {
+  let R = rgb[0] / 255;
+  let G = rgb[1] / 255;
+  let B = rgb[2] / 255;
+  let K = 1 - Math.max(R, G, B);
+  let C = (1 - R - K) / (1 - K);
+  let M = (1 - G - K) / (1 - K);
+  let Y = (1 - B - K) / (1 - K);
+  return [
+    Math.round(C * 100),
+    Math.round(M * 100),
+    Math.round(Y * 100),
+    Math.round(K * 100),
+  ];
+};
 export const hexToDec = (hexNum) => {
   const index = hexNum.length - 1;
   const alphbet = 'ABCDEF';
@@ -85,4 +131,26 @@ export const getRGB = (hex) => {
   return [hex.slice(1, 3), hex.slice(3, 5), hex.slice(5)].map((el) =>
     hexToDec(el)
   );
+};
+export const getLuminosity = (rgb) => {
+  return (0.299 * rgb[0] + 0.587 * rgb[1] + 0.114 * rgb[2]) / 255;
+};
+export const RandomHSL = (colour) => {
+  const lum = getLuminosity(getRGB(colour));
+  const hue = Math.floor(Math.random() * 360);
+  let sat, light;
+  if (lum > 0.5) {
+    sat = Math.floor(Math.random() * 100);
+    light = Math.floor(Math.random() * (12 - 0) + 0);
+  } else {
+    sat = Math.floor(Math.random() * (100 - 75) + 75);
+    light = Math.floor(Math.random() * (100 - 85) + 85);
+  }
+  return [hue, sat, light];
+};
+export const contrastColour = (colour, isRandomHue) => {
+  const lum = getLuminosity(colour);
+  const [hue, sat, light] = RandomHSL(colour);
+  if (isRandomHue) return hslToRgb(hue, sat, light);
+  return lum > 0.5 ? hslToRgb(0, 0, 0) : hslToRgb(0, 0, 100);
 };
