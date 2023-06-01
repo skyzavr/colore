5901a05367bec2252ae99b404a3cbb341c5322cd
skyzavr <dankozero111@gmail.com>
2023-06-01 14:47:49 +0300

    Add: component that calculate contrast(3 with different colours and 3 with diffetent bgr) for the colour and shows it ratio

--- src/components/pages/FullColourInfo/constrastSet/ContrastSet.js
+++ src/components/pages/FullColourInfo/constrastSet/ContrastSet.js
@@ -1,51 +1,80 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import classes from './contrastSet.module.css';
+import { RandomHSL, rgbToHex } from '../../../../convertFunctions';
+import { useCalculateContrastRatio } from '../../../../hooks/useCalculateContrastRatio';
+
 const ContrastSet = ({ colour }) => {
-  const [listOfContrasts, setListOfContrasts] = useState([
-    {
-      style: { color: '#fff', backgroundColor: colour },
-      ratio: '4.5',
-      id: Math.floor(Math.random() * 1000),
-    },
-    {
-      style: { color: '#fff', backgroundColor: colour },
-      ratio: '4.5',
-      id: Math.floor(Math.random() * 1000),
-    },
-    {
-      style: { color: '#fff', backgroundColor: colour },
-      ratio: '4.5',
-      id: Math.floor(Math.random() * 1000),
-    },
-    {
-      style: { color: colour, backgroundColor: '#000' },
-      ratio: '4.5',
-      id: Math.floor(Math.random() * 1000),
-    },
-    {
-      style: { color: colour, backgroundColor: '#000' },
-      ratio: '4.5',
-      id: Math.floor(Math.random() * 1000),
-    },
-    {
-      style: { color: colour, backgroundColor: '#000' },
-      ratio: '4.5',
-      id: Math.floor(Math.random() * 1000),
-    },
-  ]);
+  const [listOfContrasts, setListOfContrasts] = useState([]);
+  const [getRatio] = useCalculateContrastRatio();
+  const getRange = (list, hueValue) => {
+    if (list.length === 0) return true;
+    const listOfValue = [...list, hueValue].sort((a, b) => a - b);
+    for (let i = 0; i < listOfValue.length - 1; i++) {
+      let value = Math.abs(listOfValue[i] - listOfValue[i + 1]);
+      if (value < 30) return false;
+    }
+    return true;
+  };
+  const listOfCOlours = () => {
+    const colours = [];
+    const hueList = [];
+    while (hueList.length !== 3) {
+      let newColour = RandomHSL(colour);
+      let hue = newColour[0];
+      let isBigRange = getRange(hueList, hue);
+      if (isBigRange) {
+        hueList.push(hue);
+        colours.push(newColour);
+      }
+    }
+    return colours.map((el) => rgbToHex(el[0], el[1], el[2]));
+  };
+  const settingUp = (paramOne, paramTwo) => {
+    const newList = [];
+    let realList = Array.isArray(paramOne) ? paramOne.length : paramTwo.length;
+    for (let i = 0; i < realList; i++) {
+      newList.push({
+        style: {
+          color: Array.isArray(paramOne) ? paramOne[i] : paramOne,
+          bgr: Array.isArray(paramTwo) ? paramTwo[i] : paramTwo,
+        },
+        ratio: Math.floor(
+          getRatio(
+            Array.isArray(paramOne) ? paramOne[i] : paramOne,
+            Array.isArray(paramTwo) ? paramTwo[i] : paramTwo
+          )
+        ),
+        id: Math.floor(Math.random() * 1000),
+      });
+    }
+    return newList;
+  };
+  const updateListOfColours = () => {
+    const backgroundList = listOfCOlours();
+    const colourList = listOfCOlours();
+    setListOfContrasts([
+      ...settingUp(colourList, colour),
+      ...settingUp(colour, backgroundList),
+    ]);
+  };
+  useEffect(() => {
+    updateListOfColours();
+  }, [colour]);
   return (
     <div className={classes.wrapper}>
       <div className={classes.title}>Take a look at these combinations</div>
       <div className={classes.list}>
         {listOfContrasts.map((el) => (
           <div className={classes.item} key={el.id}>
-            <div style={el.style} className={classes.card}>
-              <div>Contrast ratio {el.ratio}</div>
+            <div style={{ background: el.style.bgr }} className={classes.card}>
+              <div style={{ color: el.style.color }}>
+                Contrast ratio {el.ratio}
+              </div>
             </div>
             <div className={classes.cardInfo}>
               <div className={classes.row}>
                 <div>Background Colour</div>
-                <div>{el.style.backgroundColor}</div>
+                <div>{el.style.bgr}</div>
               </div>
               <div className={classes.row}>
                 <div>Font Colour</div>
