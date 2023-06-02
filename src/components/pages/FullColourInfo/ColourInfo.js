f26db6410c331297386a969305c7d0556344d0e6
skyzavr <dankozero111@gmail.com>
2023-06-02 16:34:12 +0300

    Add: wrapper paddings

--- src/components/pages/FullColourInfo/ColourInfo.js
+++ src/components/pages/FullColourInfo/ColourInfo.js
@@ -1,16 +1,22 @@
+import { useContext } from 'react';
+import { ColourContext } from '../../../App';
+import Notification from '../../ui/notification/Notification';
 import classes from './ColourInfo.module.css';
 import ContrastSet from './constrastSet/ContrastSet';
 import Info from './infoGroup/Info';
 import Sets from './sets/Sets';
 import ShortInfo from './shortInfo/ShortInfo';
 
-const ColourInfo = ({ colour = '#F9E3CC' }) => {
+const ColourInfo = ({ color = '#F08A2C' }) => {
+  const { colour } = useContext(ColourContext);
+
   return (
     <div className={classes.wrapper}>
-      <Info colour={colour} />
-      <ShortInfo colour={colour} />
-      <Sets colour={colour} />
-      <ContrastSet colour={colour} />
+      <Info colour={color} />
+      <ShortInfo colour={color} />
+      <Sets colour={color} />
+      <ContrastSet colour={color} />
+      {Object.entries(colour).length !== 0 && <Notification param={colour} />}
     </div>
   );
 };
