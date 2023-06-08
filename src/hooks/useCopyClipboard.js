20c1412917da3994bd295eb5739644a13d6d063b
skyzavr <dankozero111@gmail.com>
2023-06-08 17:45:47 +0300

    update: upd globale variable in way whan need/don't need to be copy to cliboard

--- src/hooks/useCopyClipboard.js
+++ src/hooks/useCopyClipboard.js
@@ -1,7 +1,8 @@
 import { useContext } from 'react';
 import { ColourContext } from '../App';
+import { cmykToRgb, HexByRgb, hslToRgb } from '../convertFunctions';
 
-export function useCopyClipboard(color) {
+export function useCopyClipboard() {
   const { setColour } = useContext(ColourContext);
   async function copyToClipboard(msg) {
     let type, message;
@@ -15,14 +16,40 @@ export function useCopyClipboard(color) {
     }
     return { type, message };
   }
-  const copy = (value) => {
-    console.log(value);
-    copyToClipboard(value).then((el) => {
+  const copy = (value, type = 'HEX', isCopy = true) => {
+    if (!isCopy) {
       setColour({
-        ...el,
+        type: 'success',
+        message: `The colour ${value} has been copied!`,
+        colour: value,
         id: Math.floor(Math.random() * 10000),
       });
-    });
+    } else
+      copyToClipboard(value).then((el) => {
+        let finalValue = '';
+        const list =
+          type !== 'HEX' ? value.split(', ').map((el) => Number(el)) : value;
+        switch (type) {
+          case 'HSL':
+            const rgb = hslToRgb(list[0], list[1], list[2]);
+            finalValue = HexByRgb(rgb);
+            break;
+          case 'CMYK':
+            finalValue = HexByRgb(cmykToRgb(list));
+            break;
+          case 'RGB':
+            finalValue = HexByRgb(list);
+            break;
+          default:
+            finalValue = value;
+            break;
+        }
+        setColour({
+          ...el,
+          colour: finalValue,
+          id: Math.floor(Math.random() * 10000),
+        });
+      });
   };
   return [copy];
 }
