aecab671857bce177fa88ad7974ad7e5886018d0
skyzavr <dankozero111@gmail.com>
2023-06-08 23:26:34 +0300

    add: closing tab functionality

--- src/components/pages/FullColourInfo/infoGroup/Info.js
+++ src/components/pages/FullColourInfo/infoGroup/Info.js
@@ -1,12 +1,18 @@
 import classes from './info.module.css';
 const Info = ({ colour }) => {
+  const onCloseHandler = () => {
+    window.open('', '_self');
+    window.close();
+  };
   return (
     <div className={classes.wrapper}>
       <div className={classes.info}>
         <div className={classes.title}>Color information</div>
         <div className={classes.hexName}>{colour}</div>
       </div>
-      <div className={classes.closeBtn}>Close</div>
+      <div className={classes.closeBtn} onClick={onCloseHandler}>
+        Close
+      </div>
     </div>
   );
 };
