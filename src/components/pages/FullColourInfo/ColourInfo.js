da7df2034edf7c36c8665e4f6e1fe59f489d5639
skyzavr <dankozero111@gmail.com>
2023-06-08 23:38:09 +0300

    add: adding colour after navigate to new tab to gloval var with useContext

--- src/components/pages/FullColourInfo/ColourInfo.js
+++ src/components/pages/FullColourInfo/ColourInfo.js
@@ -1,23 +1,32 @@
-import { useContext } from 'react';
-import { ColourContext } from '../../../App';
+import { useContext, useEffect } from 'react';
+import { useCopyClipboard } from '../../../hooks/useCopyClipboard';
 import Notification from '../../ui/notification/Notification';
 import classes from './ColourInfo.module.css';
 import ContrastSet from './constrastSet/ContrastSet';
 import Info from './infoGroup/Info';
 import Sets from './sets/Sets';
 import ShortInfo from './shortInfo/ShortInfo';
+import { ColourContext } from '../../../App';
 
-const ColourInfo = ({ color = '#F08A2C' }) => {
+const ColourInfo = () => {
+  const [copy] = useCopyClipboard();
+  const color = localStorage.getItem('colour');
+  useEffect(() => {
+    copy(color, 'HEX', false);
+  }, []);
   const { colour } = useContext(ColourContext);
-
   return (
-    <div className={classes.wrapper}>
-      <Info colour={color} />
-      <ShortInfo colour={color} />
-      <Sets colour={color} />
-      <ContrastSet colour={color} />
-      {Object.entries(colour).length !== 0 && <Notification param={colour} />}
-    </div>
+    colour.colour && (
+      <div className={classes.wrapper}>
+        <Info colour={colour.colour} />
+        <ShortInfo colour={colour.colour} />
+        <Sets colour={colour.colour} />
+        <ContrastSet colour={colour.colour} />
+        {Object.entries(colour.colour).length !== 0 && (
+          <Notification param={colour.colour} />
+        )}
+      </div>
+    )
   );
 };
 export default ColourInfo;
