da33205083299b825f5a4542b694cb210e6b9d73
skyzavr <dankozero111@gmail.com>
2023-07-01 22:57:23 +0300

    upd:add methods that allows delete card, generate new colour, lock the card and get more information about the colour

--- src/components/pages/PaletteGenerator/PaletteArea/colourParams/ColourParams.js
+++ src/components/pages/PaletteGenerator/PaletteArea/colourParams/ColourParams.js
@@ -1,12 +1,82 @@
 import classes from './colourParams.module.css';
-const ColourParams = ({ colour, btns }) => {
+import GenerateIcon from '../../../../ui/Icons/GenerateIcon';
+import LockIcon from '../../../../ui/Icons/LockIcon';
+import DeleteIcon from '../../../../ui/Icons/DeleteIcon';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const ColourParams = ({ id, colour, onUpdateData }) => {
+  const btns = [
+    {
+      comp: <GenerateIcon />,
+      type: 'generate',
+      tip: 'click here to generate new colour',
+      id: Math.floor(Math.random() * 10000),
+    },
+    {
+      comp: <LockIcon />,
+      type: 'lock',
+      tip: 'click here to lock colour',
+      id: Math.floor(Math.random() * 10000),
+    },
+
+    {
+      comp: <DeleteIcon />,
+      type: 'delete',
+      tip: 'it will remmove this colour',
+      id: Math.floor(Math.random() * 10000),
+    },
+    {
+      comp: '?',
+      type: 'moreInfo',
+      tip: 'click to see more info about colour',
+      id: Math.floor(Math.random() * 10000),
+    },
+  ];
+  const [params, setParams] = useState({
+    generate: false,
+    lock: false,
+    delete: false,
+    moreInfo: false,
+  });
+  const onClickHandler = (data) => {
+    onUpdateData({ id: id, type: data });
+    if (['generate', 'delete', 'moreInfo'].some((el) => el === data)) return;
+    const copyState = { ...params };
+    if (data === 'lock') copyState.lock = !params.lock;
+    setParams(copyState);
+  };
+  const setLocalStorageColour = (value) => {
+    localStorage.setItem('colour', value);
+  };
   return (
     <div className={classes.colourParams}>
       <div className={classes.colourName}>{colour}</div>
       <div className={classes.colourBtns}>
-        {btns.map((btn) => (
-          <div className={classes.btnItem}>{btn}</div>
-        ))}
+        {btns.map((btn) =>
+          btn.type === 'moreInfo' ? (
+            <Link
+              target={'_blank'}
+              to={'/ColourInfo'}
+              onClick={() => setLocalStorageColour(colour)}
+              key={btn.id}
+            >
+              <div className={classes.btnItem}>{btn.comp}</div>
+            </Link>
+          ) : (
+            <div
+              className={[
+                `${classes.btnItem} ${
+                  params[btn.type] ? classes.locked : classes.default
+                }`,
+              ]}
+              onClick={() => onClickHandler(btn.type)}
+              key={btn.id}
+            >
+              {btn.comp}
+            </div>
+          )
+        )}
       </div>
     </div>
   );
