a0a11abdaeaeee35de542ccf68d62eb98d10d270
skyzavr <dankozero111@gmail.com>
2023-07-03 20:44:56 +0300

    upd: update view with new colourComponent

--- src/components/pages/Contrast/Contrast.js
+++ src/components/pages/Contrast/Contrast.js
@@ -1,23 +1,24 @@
-import { useState } from 'react';
-import InputColour from '../../ui/inputColour/InputColour';
+import { useEffect, useState } from 'react';
 import classes from './Contrast.module.css';
 import ContrastField from './contrastInfo/CardsField/ContrastField';
 import TextInput from './inputTextArea/TextInput';
 import UserContrastCard from './userContrastCard/UserContrastCard';
+import InputColour from '../../ui/inputColour/InputColour';
+import { generateColour } from '../../../convertFunctions';
 
-const Contrast = ({ title }) => {
-  const [colour, setColour] = useState('#242627');
-  const [bgr, setBgr] = useState('#3787FF');
+const Contrast = () => {
+  const [colour, setColour] = useState(generateColour());
+  const [bgr, setBgr] = useState(generateColour());
   const quote =
     'I found it — an eternity. This is the sun merged with the sea.';
   const [text, setText] = useState(quote);
-  const setTextArea = (data) => {
-    setText(data);
-  };
+  const setTextArea = (data) => setText(data);
+  const newColourHandler = (data) => setColour(data);
+  const newBgrHandler = (data) => setBgr(data);
   return (
     <div className={classes.wrapper}>
       <div className={classes.contrastInfo}>
-        <div className={classes.title}>{title}</div>
+        <div className={classes.title}>Contrast Checker</div>
         <div className={classes.info}>
           <p>
             Check the contrast of the text colour and the background colour.
@@ -26,26 +27,16 @@ const Contrast = ({ title }) => {
           <p>You can see it in our text or enter your own. </p>
           <p>You can also generate a color.</p>
         </div>
-        {/* Cards that shows how bad/good contrast is */}
         <ContrastField colour={colour} bgrColour={bgr} />
       </div>
       <div className={classes.contrast}>
         <div className={classes.InputField}>
           <div className={classes.colourInputField}>
-            <InputColour
-              onSetCOlour={(data) => setColour(data)}
-              text="Text colour"
-              inpColour={colour}
-            />
-            <InputColour
-              onSetCOlour={(data) => setBgr(data)}
-              text="Background colour"
-              inpColour={bgr}
-            />
+            <InputColour colour={colour} onSetColour={newColourHandler} />
+            <InputColour colour={bgr} onSetColour={newBgrHandler} />
           </div>
           <TextInput onSetText={setTextArea} quote={text} />
         </div>
-        {/* UserContrastCard - based on user's colours and text  */}
         <UserContrastCard textColour={colour} bgrColour={bgr} text={text} />
       </div>
     </div>
