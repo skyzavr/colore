2a8e655c559b1eefa639aa362dc5b6da06cafea4
skyzavr <dankozero111@gmail.com>
2023-07-03 20:48:01 +0300

    clear the key

--- src/components/pages/PaletteGenerator/PaletteArea/card/Card.js
+++ src/components/pages/PaletteGenerator/PaletteArea/card/Card.js
@@ -3,12 +3,12 @@ import ColourParams from '../colourParams/ColourParams';
 import ColourSet from '../colourSet/ColourSet';
 import classes from './Card.module.css';
 const Card = ({ onUpdate, elem }) => {
-  const { key, colour, colourSet, id } = elem;
+  const { colour, colourSet, id } = elem;
   const updatePassedData = (data) => {
     onUpdate(data);
   };
   return (
-    <div className={classes.card} key={key}>
+    <div className={classes.card}>
       <ColourCard colour={colour} />
       <ColourParams id={id} colour={colour} onUpdateData={updatePassedData} />
       <ColourSet colourSet={colourSet} />
