ac5cff04870861a3b8a9ea9751c723be77bd8487
skyzavr <dankozero111@gmail.com>
2023-07-04 17:15:22 +0300

    add: close modal window by clickin in the back

--- src/components/ui/modalWindow/ModalWindow.js
+++ src/components/ui/modalWindow/ModalWindow.js
@@ -1,8 +1,12 @@
 import classes from './ModalWindow.module.css';
-const Modal = ({ children, style }) => {
+const Modal = ({ children, style, onClose }) => {
   const { width, height } = style;
+  const onCLoseHandler = () => {
+    onClose();
+  };
   return (
     <div className={classes.wrapper}>
+      <div className={classes.back} onClick={onCLoseHandler}></div>
       <div className={classes.modal} style={{ width: width, height: height }}>
         {children}
       </div>
