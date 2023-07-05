1611be08cbd36d595079c7c8200bfa47ef8abc87
skyzavr <dankozero111@gmail.com>
2023-07-05 20:27:33 +0300

    Add: colour converter component (main wrapper)

---
+++ src/components/pages/ColourConversation/ColourConverter.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import { generateColour } from '../../../convertFunctions';
+import classes from './colourConverter.module.css';
+import Button from '../../ui/btn/button/Button';
+import ConvField from './ConverterField/ConvField';
+const ColourConverter = () => {
+  const [colour, setColour] = useState(generateColour());
+  const onGenerateColour = () => {
+    setColour(generateColour);
+  };
+  const onUpdateColourHandler = (data) => {
+    setColour(data);
+  };
+  return (
+    <div className={classes.wrapper}>
+      <div
+        className={classes.backContainer}
+        style={{ backgroundColor: `${colour}` }}
+      >
+        <div className={classes.mainContainer}>
+          <div className={classes.title}>Colour converter</div>
+          <div className={classes.desc}>
+            To convert a colour, change any value and it will automatically
+            recalculate, or just choose a random colour!
+          </div>
+          <ConvField
+            initColour={colour}
+            onUpdateColour={onUpdateColourHandler}
+          />
+          <div className={classes.btn}>
+            <Button
+              text="Random colour"
+              type="border"
+              onClickFunc={onGenerateColour}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ColourConverter;
