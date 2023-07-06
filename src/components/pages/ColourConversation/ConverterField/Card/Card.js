0c0ff93758cb8773427ccfc117815e406310f6f0
skyzavr <dankozero111@gmail.com>
2023-07-06 18:08:50 +0300

    Add: card input component

---
+++ src/components/pages/ColourConversation/ConverterField/Card/Card.js
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react';
+import InputNummber from '../InputNumber/InputNummber';
+import classes from './card.module.css';
+
+const Card = ({ valueList, onUpdateValueListHandler }) => {
+  const { title, params, maxValue } = valueList;
+  const [list, setList] = useState(params);
+  const onUpdateValuesList = (data) => {
+    const { ind, el } = data;
+    const dupList = [...list];
+    dupList[ind] = Number(el);
+    setList(dupList);
+    onUpdateValueListHandler({ type: title, dupList });
+  };
+  useEffect(() => {
+    setList(params);
+  }, [params]);
+  return (
+    <div className={classes.card}>
+      <div className={classes.title}>{title}</div>
+      <div className={classes.itemInputs}>
+        {list.map((el, ind) => (
+          <div className={classes.item}>
+            <InputNummber
+              initialValue={el}
+              ind={ind}
+              maxValue={maxValue[ind]}
+              onUpdateValue={onUpdateValuesList}
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Card;
